8e2d192f213ee0a27195c5e9530d74568b752a41
Anna Kalinina <annakalinina13071993@gmail.com>
2021-06-07 21:50:56 +0300

    правки после проверки

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/* eslint-disable prefer-template */
+/* eslint-disable arrow-parens */
+/* eslint-disable prefer-const */
 /* eslint-disable arrow-body-style */
 /* eslint-disable id-length */
 /* eslint-disable no-unused-vars */
@@ -23,24 +26,11 @@ const getRandomFractionalNumber = (min, max ,numberAfterDecimalPoint = 5) => {
 
 const getRandomArrayElement = (array)  => array[getRandomNumber(0,array.length-1)];
 
-//создание подъобъектов
+// функция генерирующая случайный массив
 
-const IMAGE_ADRESS = [
-  'img/avatars/user01.png',
-  'img/avatars/user02.png',
-  'img/avatars/user03.png',
-  'img/avatars/user04.png',
-  'img/avatars/user05.png',
-  'img/avatars/user06.png',
-  'img/avatars/user07.png',
-  'img/avatars/user08.png',
-] ;
+const getRandomArray = (array) => array.filter( () => getRandomNumber(0,1) );
 
-const createAuthor =() => {
-  return {
-    avatar: getRandomArrayElement(IMAGE_ADRESS),
-  };
-};
+// входные данные
 
 const TITLES = [
   'One-room apartment',
@@ -51,18 +41,13 @@ const TITLES = [
   'Tanhouse',
   'Residential one-storey house',
 ];
-const createTitle = () =>  getRandomArrayElement(TITLES);
+const INDEX = 8;
 const MIN_X=35.65;
 const MAX_X=35.7;
 const MIN_Y=139.7;
 const MAX_Y=139.8;
-const createAddress =() => [
-  getRandomFractionalNumber(MIN_X,MAX_X),
-  getRandomFractionalNumber(MIN_Y,MAX_Y),
-];
 const MIN_PRICE=0;
 const MAX_PRICE = 10000000;
-const createPrice = () => getRandomNumber(MIN_PRICE, MAX_PRICE);
 const TYPES = [
   'palace',
   'flat',
@@ -70,18 +55,13 @@ const TYPES = [
   'bungalow',
   'hotel',
 ];
-const createType = () => getRandomArrayElement(TYPES);
 const MIN_ROOMS=1;
 const MAX_ROOMS=10;
-const createRooms = () => getRandomNumber(MIN_ROOMS, MAX_ROOMS);
 const MIN_GUETS=1;
 const MAX_GUETS=10;
-const createGuests = () => getRandomNumber(MIN_GUETS, MAX_GUETS);
 const CHECKTIMES = [
   '12:00', '13:00', '14:00',
 ];
-const createCheckIn = () => getRandomArrayElement(CHECKTIMES);
-const createCheckOut = () => getRandomArrayElement(CHECKTIMES);
 const FEATURES = [
   'wifi',
   'dishwasher',
@@ -90,71 +70,69 @@ const FEATURES = [
   'elevator',
   'conditioner',
 ];
-
-// функция по возвращению массива со случайным количесвом элементов
-const getRandomArray = (array) => {
-  const randomArray = [];
-  for (let i = 0; i < getRandomNumber(0, array.length); i++) {
-    randomArray[i] =getRandomArrayElement[array];
-    if (randomArray[i] !== randomArray[i-1]) {
-      randomArray[i];
-    }
-  }
-  return randomArray;
-};
-
-const createFeatures = () => getRandomArray(FEATURES);
 const DESCRIPTIONS = [
   'Bright apartment with windows on the sunny side',
   'Huge apartment with a balcony',
   'House with a garden',
   'Located not far from the metro',
 ];
-const createDescription = () => getRandomArrayElement(DESCRIPTIONS);
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
-const createPhotos = () => getRandomArray(PHOTOS);
-const createLocation = () => {
-  return {
+
+// создание объекта для ондого элемента массива
+
+const createObject = () => {
+  const createAuthor = (index) => {
+    return {
+      avatar: `img/avatars/user0${getRandomNumber(1,index)}.png`,
+    };
+  };
+  const features = getRandomArray(FEATURES);
+  const checkin = getRandomArrayElement(CHECKTIMES);
+  const checkout = checkin;
+  const description = getRandomArrayElement(DESCRIPTIONS);
+  const photos = getRandomArray(PHOTOS);
+  const title = getRandomArrayElement(TITLES);
+  const type =  getRandomArrayElement(TYPES);
+  const rooms = getRandomNumber(MIN_ROOMS, MAX_ROOMS);
+  const guests = getRandomNumber(MIN_GUETS, MAX_GUETS);
+  const price =  getRandomNumber(MIN_PRICE, MAX_PRICE);
+  const address ={
     lat: getRandomFractionalNumber(MIN_X,MAX_X) ,
     lng: getRandomFractionalNumber(MIN_Y, MAX_Y),
   };
-};
+  const location = address;
 
-
-// создание объекта Offer
-
-const createOffer = () => {
   return {
-    title : createTitle() ,
-    adress: createAddress() ,
-    price: createPrice() ,
-    type: createType() ,
-    rooms: createRooms() ,
-    guests: createGuests() ,
-    checkin: createCheckIn() ,
-    checkout: createCheckOut() ,
-    features: createFeatures() ,
-    description: createDescription() ,
-    photos: createPhotos() ,
+    author: createAuthor(INDEX) ,
+    offer:  {
+      title ,
+      address ,
+      price,
+      type ,
+      rooms,
+      guests,
+      checkin,
+      checkout,
+      features,
+      description,
+      photos,
+    } ,
+    location,
   };
 };
 
+// функция по созданию массива из index объектов
 
-// наполнение одного элемента массива в виде объекта
-
-const createObject = () => {
-  return {
-    author: createAuthor() ,
-    offer: createOffer() ,
-    location: createLocation() ,
-  };
+const dataGeneration =(index) =>  {
+  let newArray =[];
+  for ( let i=0; i< index; i++){
+    newArray[i] = createObject();
+  }
+  return newArray;
 };
 
-// создание массива из 10 объектов
-
-const dataGeneration = Array(10).fill(createObject());
-
+dataGeneration(10);
