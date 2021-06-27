d67e94e65bafbab135de7a151940a125dcef519f
Anna Kalinina <annakalinina13071993@gmail.com>
2021-06-27 14:05:13 +0300

    сделала фунции с коллбэком

--- js/main.js
+++ js/main.js
@@ -2,13 +2,18 @@ import { createObjects } from './data.js';
 import { renderCard } from './card.js';
 import { getValidation } from './form.js';
 import { disableForm, enableForm } from './page.js';
-import { initailizeMap } from './map.js';
+import { initailizeMap, getMainPinIcon, getMarkers} from './map.js';
 const COUNT = 8;
 const objects = createObjects(COUNT);
 
 
 disableForm();
-if (initailizeMap()) { enableForm(); }
+initailizeMap(() => {
+  enableForm();
+  getMainPinIcon();
+  getMarkers(objects, renderCard);
+});
+
 getValidation();
 
 
--- js/map.js
+++ js/map.js
@@ -1,15 +1,6 @@
-
-/* global L:readonly */
-
-import { createObjects } from './data.js';
-import { renderCard } from './card.js';
-
-const COUNT = 8;
-const objects = createObjects(COUNT);
-const address = document.querySelector('#address');
-
-const initailizeMap = () => {
-  const map = L.map('map-canvas')
+let map;
+function initailizeMap(callback) {
+  map = L.map('map-canvas')
     .setView({
       lat: 35.6894,
       lng: 139.69224,
@@ -20,7 +11,11 @@ const initailizeMap = () => {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     },
   ).addTo(map);
+  map.on('load', callback());
 
+}
+
+const getMainPinIcon = () => {
   const mainPinIcon = L.icon({
     iconUrl: '../img/main-pin.svg',
     iconSize: [52, 52],
@@ -38,37 +33,47 @@ const initailizeMap = () => {
     },
   );
   mainPinMarker.addTo(map);
-
+  const address = document.querySelector('#address');
   mainPinMarker.on('moveend', (evt) => {
     const addressPoint = evt.target.getLatLng();
     address.value = `${addressPoint.lat.toFixed(5)},${addressPoint.lng.toFixed(5)}`;
   });
-
-  objects.forEach((object) => {
-    const lat = Number(object.location.lat);
-    const lng = Number(object.location.lng);
-    const icon = L.icon({
-      iconUrl: '../img/pin.svg',
-      iconSize: [40, 40],
-      iconAnchor: [20, 40],
-    });
-
-    const marker = L.marker(
-      {
-        lat,
-        lng,
-      },
-      {
-        icon,
-      },
-    );
-
-    marker.addTo(map)
-      .bindPopup(renderCard(object),
-        {
-          keepInView: true,
-        });
-  });
-  if (map.on('load')) { return true; }
 };
-export { initailizeMap };
+
+
+const createMarker = (object, renderCard) => {
+  const markerGroup = L.layerGroup().addTo(map);
+  const lat = Number(object.location.lat);
+  const lng = Number(object.location.lng);
+  const icon = L.icon({
+    iconUrl: '../img/pin.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+  const marker = L.marker(
+    {
+      lat,
+      lng,
+    },
+    {
+      icon,
+    },
+  );
+
+  marker.addTo(markerGroup)
+    .bindPopup(renderCard(object),
+      {
+        keepInView: true,
+      });
+};
+
+const getMarkers = (objects, renderCard) => {
+  objects.forEach((object) => {
+    createMarker(object, renderCard);
+  });
+
+};
+
+export { initailizeMap, getMainPinIcon, getMarkers };
+
